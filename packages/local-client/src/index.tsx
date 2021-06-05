f5e0f291713ef5a23c164ab9962c707ca4b9711f
LordLuceus <lord.luceus@gmail.com>
2021-06-05 11:04:47 +0200

    Implement in-browser transpiling

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
 
 const App = () => {
-  return <h1>Hello there!</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const initialize = async () => {
+    await esbuild.initialize({
+      wasmURL: "/esbuild.wasm"
+    });
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
+  const onClick = async () => {
+    const result = await esbuild.transform(input, {
+      loader: "jsx",
+      target: "es2015"
+    });
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input} />
+      <button onClick={onClick}>Submit</button>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
