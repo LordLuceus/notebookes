492e3b6f95fc5ed9f0949ed64e5a0057092f5447
LordLuceus <lord.luceus@gmail.com>
2021-06-07 13:45:56 +0200

    Handle resolution of nested paths

--- packages/local-client/src/plugins/unpkg-path-plugin.ts
+++ packages/local-client/src/plugins/unpkg-path-plugin.ts
@@ -10,17 +10,15 @@ export const unpkgPathPlugin = () => {
 
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-          // } else if (args.path === "tiny-test-pkg") {
-          //   return {
-          //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-          //     namespace: "a"
-          //   };
         }
 
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href
           };
         }
 
@@ -34,17 +32,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from "medium-test-pkg";
+              import message from "nested-test-pkg";
               console.log(message);
             `
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
 
         return {
           loader: "jsx",
-          contents: data
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname
         };
       });
     }
