acaa8050b411fd292776dae337cf124e8b2fed28
LordLuceus <lord.luceus@gmail.com>
2021-06-07 11:49:02 +0200

    Add ESBuild plugin to help with bundling in the browser

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -17,11 +18,13 @@ const App = () => {
   }, []);
 
   const onClick = async () => {
-    const result = await esbuild.transform(input, {
-      loader: "jsx",
-      target: "es2015"
+    const result = await esbuild.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     });
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   return (
