e7f82805edb4a8c2d64ffd3455b1bea3464924d5
LordLuceus <lord.luceus@gmail.com>
2021-06-10 10:04:39 +0200

    Add support for css files

--- packages/local-client/src/plugins/fetch-plugin.ts
+++ packages/local-client/src/plugins/fetch-plugin.ts
@@ -1,43 +1,65 @@
 import * as esbuild from "esbuild-wasm";
-import localForage from "localforage";
 import axios from "axios";
+import localForage from "localforage";
 
 const fileCache = localForage.createInstance({
-  name: "file-cache"
+  name: "filecache"
 });
 
-export const fetchPlugin = (code: string) => {
+export const fetchPlugin = (input: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: code
-          };
-        }
-
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
-
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /^index\.js$/ }, (args: esbuild.OnLoadArgs) => {
+        return {
+          loader: "jsx",
+          contents: input
+        };
+      });
+
+      build.onLoad({ filter: /\.css$/ }, async (args: esbuild.OnLoadArgs) => {
         const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement("style");
+          style.innerText = ${escaped};
+          document.head.appendChild(style);
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents,
           resolveDir: new URL("./", request.responseURL).pathname
         };
 
         await fileCache.setItem(args.path, result);
+        return result;
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
+        const { data, request } = await axios.get(args.path);
+        const contents = data;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname
+        };
+
+        await fileCache.setItem(args.path, result);
         return result;
       });
     }
