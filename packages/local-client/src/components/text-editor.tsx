4263945dd5e1ee7e84d0d8dd76598473a8816781
LordLuceus <lord.luceus@gmail.com>
2021-06-14 13:59:33 +0200

    Implement preview and edit modes for the markdown editor

--- packages/local-client/src/components/text-editor.tsx
+++ packages/local-client/src/components/text-editor.tsx
@@ -1,9 +1,43 @@
 import MDEditor from "@uiw/react-md-editor";
+import { useEffect, useRef, useState } from "react";
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  });
+
+  if (editing) {
+    return (
+      <div ref={ref} className="text-editor">
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source="# Header" />
+      </div>
     </div>
   );
 };
