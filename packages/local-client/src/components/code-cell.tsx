7d0fe454feecd7d68341142c8cc77addcf89a6ec
LordLuceus <lord.luceus@gmail.com>
2021-06-18 05:09:00 +0200

    Fix content being shown in all cells after the current one

--- packages/local-client/src/components/code-cell.tsx
+++ packages/local-client/src/components/code-cell.tsx
@@ -18,28 +18,37 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode: string[] = [
-      `
-        import _React from "react";
-        import _ReactDOM from "react-dom";
+    const showFunc = `
+      import _React from "react";
+      import _ReactDOM from "react-dom";
 
-        const show = (value) => {
-          const root = document.querySelector("#root");
+      var show = (value) => {
+        const root = document.querySelector("#root");
 
-          if (typeof value === "object") {
-            if (value.$$typeof && value.props) {
-              _ReactDOM.render(value, root);
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
+        if (typeof value === "object") {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
           } else {
-            document.querySelector("#root").innerHTML = value;
+            root.innerHTML = JSON.stringify(value);
           }
-        };
-      `,
-    ];
+        } else {
+          root.innerHTML = value;
+        }
+      };
+    `;
+
+    const showFuncNoOp = `
+      var show = () => {};
+    `;
+
+    const cumulativeCode: string[] = [];
     orderedCells.some((c) => {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoOp);
+        }
         cumulativeCode.push(c.content);
       }
       return c.id === cell.id;
