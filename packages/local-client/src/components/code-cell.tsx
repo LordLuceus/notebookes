168311b9f88fd1c65eaa18436e3593311c4a1868
LordLuceus <lord.luceus@gmail.com>
2021-06-18 03:58:09 +0200

    Bundle the cumulative code

--- packages/local-client/src/components/code-cell.tsx
+++ packages/local-client/src/components/code-cell.tsx
@@ -29,23 +29,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return cumulativeCode;
   });
 
-  console.log(cumulativeCode);
-
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
 
   const handleEditorChange = (value: string | undefined): void => {
     if (value) {
