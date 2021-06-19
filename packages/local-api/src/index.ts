a46c57b48575d6ebf8e69b095a27c2a87a17cf75
LordLuceus <lord.luceus@gmail.com>
2021-06-19 15:14:28 +0200

    Communicate data to the local api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,1 +1,5 @@
-export default () => console.log("Server started.");
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log(
+    `Serving traffic on port ${port}. Editing ${filename}, which is in ${dir}.`
+  );
+};
